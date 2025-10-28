0161b5487057a23da04de6445397772af0d97d27
enten0103 <enten@gmail.com>
2025-10-28 22:26:22 +0800

    feat(andorid):优化安卓返回

--- android/app/src/main/kotlin/com/example/tono_music/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tono_music/MainActivity.kt
@@ -2,4 +2,8 @@ package com.example.tono_music
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+	override fun onBackPressed() {
+		moveTaskToBack(true)
+	}
+}
